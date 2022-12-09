44246b55913b8c83f4749ad3384443280b1c9497
mbukowski <mateusz.bukowski@wakacje.pl>
2022-12-09 11:29:35 +0100

    nav

--- navigation/Navigation.js
+++ navigation/Navigation.js
@@ -3,6 +3,10 @@ import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { NavigationContainer, DefaultTheme, DarkTheme, useNavigation } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import * as React from 'react';
+import Email from '../components/SvgIcons/Email';
+import HomeIcon from '../components/SvgIcons/HomeIcon';
+import Person from '../components/SvgIcons/Person';
+import Star from '../components/SvgIcons/Star';
 import Home from '../screens/Home';
 import Login from '../screens/Login';
 import Notification from '../screens/Notification';
@@ -23,10 +27,52 @@ export default function Navigation() {
 function HomeTab() {
   return (
     <Tab.Navigator>
-      <Tab.Screen name="Home" component={Home} />
-      <Tab.Screen name="Profile" component={Profile} />
-      <Tab.Screen name="Ranking" component={Ranking} />
-      <Tab.Screen name="Notification" component={Notification} />
+      <Tab.Screen 
+      name="Home" 
+      component={Home} 
+      options={{
+        title: 'Home',
+        headerShown: false,
+        tabBarLabelPosition:'below-icon',
+        tabBarIcon: ({size,focused,color}) => {
+          return (
+          <HomeIcon/>
+          );
+        },
+      }}
+      />
+      <Tab.Screen name="Profile" component={Profile}
+        options={{
+          title: 'Profil',
+          headerShown: false,
+          tabBarIcon: ({size,focused,color}) => {
+            return (
+<Person/>
+            );
+          },
+        }}
+       />
+      <Tab.Screen name="Ranking" component={Ranking} 
+        options={{
+          title: 'Ranking',
+          headerShown: false,
+          tabBarIcon: ({size,focused,color}) => {
+            return (
+              <Star/>
+            );
+          },
+        }}
+      />
+      <Tab.Screen name="Notification" component={Notification}
+        options={{
+          title: 'Powiadomienia',
+          headerShown: false,
+          tabBarIcon: ({size,focused,color}) => {
+            return (
+<Email/>
+            );
+          },
+        }} />
     </Tab.Navigator>
   );
 }
@@ -34,8 +80,8 @@ function HomeTab() {
 function RootNavigator() {
   return (
     <Stack.Navigator>
-      <Stack.Screen name="Login" component={Login} options={{ headerShown: false }} />
       <Stack.Screen name="HomeTab" component={HomeTab} options={{ headerShown: false }} />
+      <Stack.Screen name="Login" component={Login} options={{ headerShown: false }} />
     </Stack.Navigator>
   );
 }
