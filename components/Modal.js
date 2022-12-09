afc17d7d6152e1c285d5d66c27d0d2a746e7a5e8
mbukowski <mateusz.bukowski@wakacje.pl>
2022-12-09 16:01:33 +0100

    MOdla

--- components/Modal.js
+++ components/Modal.js
@@ -8,11 +8,12 @@ import {
   Button,
   Divider,
   useTheme,
+  TextInput,
 } from "react-native-paper";
+import CloseIcon from "./SvgIcons/CloseIcon";
 
-const Modal = () => {
-  const { navigate } = useNavigation();
-  const { height, width } = useWindowDimensions();
+const Modal = ({ children }) => {
+  const { width } = useWindowDimensions();
   const theme = useTheme();
 
   const styles = StyleSheet.create({
@@ -26,87 +27,12 @@ const Modal = () => {
       borderTopLeftRadius: 12,
       borderTopRightRadius: 12,
     },
-    title: {
-      marginLeft: "auto",
-      marginRight: "auto",
-      marginBottom: 16,
-      fontWeight: "700",
-      fontSize: 16,
-    },
-    divider: {
-      backgroundColor: theme.colors.darkGray,
-    },
-    buttonWrapper: {
-      paddingTop: 10,
-      paddingBottom: 10,
-      flexDirection: "row",
-      justifyContent: "space-between",
-    },
-    buttonContentStyle: {
-      height: 50,
-      width: 170,
-    },
-    labelStyle: {
-      margin: "auto",
-    },
-    button: {
-      borderRadius: 10,
-      width: 170,
-      height: 50,
-    },
-    outlinedContent: {
-      color: theme.colors.darkGray,
-      fontWeight: "600",
-    },
-    containedButton: {
-      backgroundColor: theme.colors.darkGray,
-    },
-    containedLabel: {
-      color: "#FFD024",
-      fontWeight: "600",
-    },
   });
 
   return (
     <Portal>
       <PaperModal visible contentContainerStyle={styles.contentContainerStyle}>
-        <View />
-        <Text style={styles.title}>Wyślij zaproszenie do gry</Text>
-        <View />
-        <Divider bold style={styles.divider} />
-        <Text variant="titleMedium">
-          Już wiesz z kim chcesz zagrać? Wyszukaj przeciwnika i wyślij mu
-          zaproszenie.
-        </Text>
-        <Text variant="titleSmall">
-          ...możesz też wybrać kogoś z ostatnich przeciwników:
-        </Text>
-        <Divider bold style={styles.divider} />
-        <View style={styles.buttonWrapper}>
-          <Button
-            mode="outlined"
-            style={styles.button}
-            contentStyle={styles.buttonContentStyle}
-            labelStyle={[styles.labelStyle, styles.outlinedContent]}
-          >
-            Anuluj
-          </Button>
-          <Button
-            mode="contained"
-            style={styles.button}
-            contentStyle={[
-              styles.buttonContentStyle,
-              { backgroundColor: "#323232" },
-            ]}
-            labelStyle={[
-              styles.labelStyle,
-              styles.containedLabel,
-              { marginHorizontal: 0 },
-            ]}
-          >
-            Wyślij zaproszenie
-          </Button>
-        </View>
+        {children}
       </PaperModal>
     </Portal>
   );
--- screens/Profile.js
+++ screens/Profile.js
@@ -1,12 +1,12 @@
 import React from "react";
 import { View, Text } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
-import Modal from "../components/Modal";
+import SendInvitationModal from "../components/Modals/SendInvitationModal";
 
 const Profile = () => {
   return (
     <SafeAreaView>
-      <Modal />
+      <SendInvitationModal />
     </SafeAreaView>
   );
 };
